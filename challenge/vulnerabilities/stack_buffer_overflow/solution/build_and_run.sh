c6a77e4b2ffdd240d5708e330d6bcf7719d162fa
Hcamael <Hcamael@0x48.pw>
2019-02-13 16:41:49 +0800

    modify build

--- challenge/vulnerabilities/stack_buffer_overflow/solution/build_and_run.sh
+++ challenge/vulnerabilities/stack_buffer_overflow/solution/build_and_run.sh
@@ -1,7 +1,6 @@
 EXPLOIT_NAME=stack_buffer_overflow_exploit
-cp /mnt/hgfs/tmp/android_kernel/pwn/challenges/stack_buffer_overflow/solution/jni/stack_buffer_overflow_exploit.c ./jni
 ~/android_sdk/ndk-bundle/ndk-build
 # ndk-build
-adb push ./libs/armeabi-v7a/$EXPLOIT_NAME /data/local/tmp/
-adb shell chmod 777 /data/local/tmp/$EXPLOT_NAME
+~/android_sdk/platform-tools/adb push ./libs/armeabi-v7a/$EXPLOIT_NAME /data/local/tmp/
+~/android_sdk/platform-tools/adb shell chmod 777 /data/local/tmp/$EXPLOT_NAME
 # adb shell /data/local/tmp/$EXPLOIT_NAME
